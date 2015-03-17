9b827ae8ce3549ee2bcdf9a4ec90ba798808ac89
Hani Sayegh <papaaced@gmail.com>
2015-03-16 20:03:48 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "Board.h"
+#include "Simulator.h"
+
+int main()
+{
+    Board brd;
+    std::cout << brd << std::endl;
+
+    Simulator sim;
+
+
+    return 0;
+}
